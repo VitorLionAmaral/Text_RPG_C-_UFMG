a796f3a722ddfe9c3339c2858ccc79addffda093
M4th3usASR <163683980+M4th3usASR@users.noreply.github.com>
2024-07-13 20:31:26 -0300

    Update inventario.cpp

--- codigo/inventario.cpp
+++ codigo/inventario.cpp
@@ -8,9 +8,6 @@ void Inventario::adicionarItem(const item& novoItem) {
 void Inventario::removerItem(int indice) {
     if (indice >= 1 && indice <= static_cast<int>(itens.size())) {
         itens.erase(itens.begin() + indice - 1);
-        std::cout << "Item removido com sucesso.\n";
-    } else {
-        std::cout << "Índice inválido. Nenhum item removido.\n";
     }
 }
 
@@ -20,12 +17,24 @@ void Inventario::exibirInventario() {
     } else {
         std::cout << "Inventário:\n";
         for (size_t i = 0; i < itens.size(); ++i) {
+            std::cout << "[" << i + 1 << "] "; 
             itens[i].imprimirBonus(); 
             std::cout << "---------------------------\n";
         }
     }
 }
 
+item Inventario::getItem(int index) const {
+    if (index >= 0 && index < itens.size()) {
+        return itens[index];
+    } else {
+        throw std::out_of_range("Índice fora do intervalo");
+    }
+}
 void Inventario::limparInventario() {
     itens.clear();
+}
+
+int Inventario::getTotalItens() const {
+    return itens.size(); 
 }
